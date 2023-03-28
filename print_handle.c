5266431730a926797a0c81d5ab5aebc5277c40b9
Bryanf20 <fongangbryan@gmail.com>
2023-03-28 16:40:51 -0700

    edited some files

--- print_handle.c
+++ print_handle.c
@@ -11,7 +11,6 @@
  * @size: Size specifier
  * Return: 1 or 2;
  */
-
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -24,33 +23,23 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-	{
 		if (fmt[*ind] == fmt_types[i].fmt)
-		{
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-		}
+
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
-		{			
 			return (-1);
-		}
 		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-		{
 			unknow_len += write(1, " ", 1);
-		}
 		else if (width)
 		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-			{
 				--(*ind);
-			}
 			if (fmt[*ind] == ' ')
-			{
 				--(*ind);
-			}
 			return (1);
 		}
 		unknow_len += write(1, &fmt[*ind], 1);
